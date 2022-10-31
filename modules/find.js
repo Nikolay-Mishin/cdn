557492d97145ed31138279861d34f9741c71fede
Nikolay_Mishin <mishin.nikolay.d3@mail.ru>
2022-10-31 16:00:48 +0300

    Add findDuplicates

---
+++ modules/find.js
@@ -0,0 +1,17 @@
+export const findDuplicates = (arr) => {
+	let sorted_arr = arr.slice().sort(); // You can define the comparing function here.
+	// JS by default uses a crappy string compare.
+	// (we use slice to clone the array so the
+	// original array won't be modified)
+	let results = [];
+	for (let i = 0; i < sorted_arr.length - 1; i++) {
+		if (sorted_arr[i + 1] == sorted_arr[i]) {
+			results.push(sorted_arr[i]);
+		}
+	}
+	return results;
+}
+
+const columnList = [...document.getElementsByClassName('fl-detailed_table_sidebar-column')].map((item) => item.innerText)
+
+console.log(findDuplicates(columnList))
